f981b6262437462e7984423213fbd1dbf74eafe0
om kar shukla <shuklaomkar3010@gmail.com>
2025-07-23 03:03:20 +0530

    made products page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
         </Routes>
         <Footer />
       </Router>
