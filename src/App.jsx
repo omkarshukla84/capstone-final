3efd1752e2ec1d4b59757234b2e01e80a8e7e85e
om kar shukla <shuklaomkar3010@gmail.com>
2025-07-23 22:22:35 +0530

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
 import Destinations from "./components/pages/Destinations";
 import SignUp from "./components/pages/SignUp";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/products" exact component={Products} />
           <Route path="/destinations" exact component={Destinations} />
           <Route path="/sign-up" exact component={SignUp} />
+          <Route path="/login" exact component={Login} />
         </Routes>
         <Footer />
       </Router>
